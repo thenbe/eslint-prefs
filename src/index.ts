e1bfba44ef115bd286b0838158fb3dca5c500c65
Naser Aleisa <aleisa.naser@gmail.com>
2023-11-23 03:19:57 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import type { FlatConfigItem } from '@antfu/eslint-config'
+
+export const configs = {
+  global: {
+    name: 'global-rules',
+    rules: {
+      'curly': 'off', // if conditions look more clear with curly braces
+      'no-useless-return': 'off', // potential footgun when refactoring
+      'sort-imports': 'off', // handled by perfectionist
+      'perfectionist/sort-imports': 'error',
+    },
+  },
+  tsconfig: {
+    files: ['**/tsconfig.json'],
+    rules: {
+      'jsonc/sort-keys': 'off',
+    },
+  },
+  svelte: {
+    files: ['**/*.svelte'],
+    rules: {
+      'no-undef-init': 'off', // TODO: re-evaluate for svelte v5
+    },
+  },
+} satisfies Record<string, FlatConfigItem>
+
+export const all = Object.values(configs)
